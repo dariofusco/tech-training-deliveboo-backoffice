104d99dd82b82b6e83b3c7ab6c5d10e5ede6b83f
Giuseppe Indomenico <pippo.son@hotmail.com>
2024-01-24 12:15:26 +0100

    fix margin in card and add bold

--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -7,15 +7,18 @@
             <a href={{ route('admin.restaurant.create') }} class="btn btn-primary">Crea ristorante</a>
         </div>
     @else
-        <div class="container text-center">
-            <h1>Il tuo ristorante</h1>
-        </div>
-        <div class="container d-flex justify-content-center mt-2">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="{{ $userRestaurant->photo }}" alt="{{ $userRestaurant->name }}">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $userRestaurant->name }}</h5>
-                    <p class="card-text">Indirizzo: {{ $userRestaurant->address }}</p>
+        <div class="container mt-3">
+
+            <div class="text-center mb-3">
+                <h1>Il tuo ristorante</h1>
+            </div>
+            <div class="d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="{{ $userRestaurant->photo }}" alt="{{ $userRestaurant->name }}">
+                    <div class="card-body text-center ">
+                        <h5 class="card-title fw-bolder fs-3 mb-2">{{ $userRestaurant->name }}</h5>
+                        <p class="card-text"> <span class="fw-bold">Indirizzo:</span> {{ $userRestaurant->address }}</p>
+                    </div>
                 </div>
             </div>
         </div>
