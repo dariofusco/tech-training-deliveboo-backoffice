94ce6dd5e6099feefcce7e770ab0f45a6a9b4dc6
Giuseppe Indomenico <pippo.son@hotmail.com>
2024-01-24 11:40:05 +0100

    indetation indec and add style create

--- resources/views/restaurant/create.blade.php
+++ resources/views/restaurant/create.blade.php
@@ -1,37 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-
-@if ($userRestaurant === null)
-<div class="container">
-    <form action="{{ route('admin.restaurant.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="mb-3">
-            <label for="name" class="form-label">Nome</label>
-            <input type="text" class="form-control" name='name'>
+    @if ($userRestaurant === null)
+        <div class="container mt-5">
+            <form action="{{ route('admin.restaurant.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="mb-5">
+                    <label for="name" class="form-label">Nome</label>
+                    <input type="text" class="form-control" name='name'>
+                </div>
+                <div class="mb-5">
+                    <label for="address" class="form-label">Indirizzo</label>
+                    <input type="text" class="form-control" name='address'>
+                </div>
+                <div class="mb-5">
+                    <label for="piva" class="form-label">P.Iva</label>
+                    <input type="text" class="form-control" name='piva'>
+                </div>
+                <div class="mb-5">
+                    <label for="photo" class="form-label">Foto</label>
+                    <input type="file" class="form-control" name='photo'>
+                </div>
+                <button type="submit" class="btn btn-primary">Crea Ristorante</button>
+            </form>
         </div>
-        <div class="mb-3">
-            <label for="address" class="form-label">Indirizzo</label>
-            <input type="text" class="form-control" name='address'>
+    @else
+        <div class="container d-flex align-items-center flex-column">
+            <h1>Il tuo account ha già un ristorante, non puoi crearne altri</h1>
+            <a href={{ route('admin.restaurant.index') }} class="btn btn-primary d-block">Vai al tuo ristorante</a>
         </div>
-        <div class="mb-3">
-            <label for="piva" class="form-label">P.Iva</label>
-            <input type="text" class="form-control" name='piva'>
-        </div>
-        <div class="mb-3">
-            <label for="photo" class="form-label">Foto</label>
-            <input type="file" class="form-control" name='photo'>
-        </div>
-        <button type="submit" class="btn btn-primary">Crea Ristorante</button>
-    </form>
-</div>
-@else
-
-<div class="container d-flex align-items-center flex-column">
-    <h1>Il tuo account ha già un ristorante, non puoi crearne altri</h1>
-    <a href={{route('admin.restaurant.index')}} class="btn btn-primary d-block">Vai al tuo ristorante</a>
-</div>
-
-@endif
-
+    @endif
 @endsection
--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -1,26 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-@if ($userRestaurant === null)
-<div class="container d-flex align-items-center flex-column">
-    <h1>Non hai ancora creato un ristorante</h1>
-    <a href={{route('admin.restaurant.create')}} class="btn btn-primary d-block">Crea ristorante</a>
-</div>
-
-@else
-
-<div class="container text-center">
-    <h1>Il tuo ristorante</h1>
-</div>
-    <div class="container d-flex justify-content-center mt-2">
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="{{$userRestaurant->photo}}" alt="{{$userRestaurant->name}}">
-            <div class="card-body">
-              <h5 class="card-title">{{$userRestaurant->name}}</h5>
-              <p class="card-text">Indirizzo: {{$userRestaurant->address}}</p>
+    @if ($userRestaurant === null)
+        <div class="container d-flex align-items-center flex-column">
+            <h1 class="my-4 fw-bold">Non hai ancora creato un ristorante</h1>
+            <a href={{ route('admin.restaurant.create') }} class="btn btn-primary">Crea ristorante</a>
+        </div>
+    @else
+        <div class="container text-center">
+            <h1>Il tuo ristorante</h1>
+        </div>
+        <div class="container d-flex justify-content-center mt-2">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="{{ $userRestaurant->photo }}" alt="{{ $userRestaurant->name }}">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $userRestaurant->name }}</h5>
+                    <p class="card-text">Indirizzo: {{ $userRestaurant->address }}</p>
+                </div>
             </div>
-          </div>
-    </div>
+        </div>
     @endif
-
 @endsection
