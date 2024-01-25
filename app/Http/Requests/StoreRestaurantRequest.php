5b9da0840b70f4aa15eccbd7a5083ced936a1b1c
raffaelecapaldo <raffaelecpl@gmail.com>
2024-01-25 03:11:11 +0100

    Added errors and localized messages for create-restaurant

--- app/Http/Requests/StoreRestaurantRequest.php
+++ app/Http/Requests/StoreRestaurantRequest.php
@@ -22,11 +22,28 @@ class StoreRestaurantRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required',
-            'address' => 'required',
+            'name' => 'required|min:3|max:200',
+            'address' => 'required|max:200',
             'piva' => 'required|numeric',
             'photo' => 'required|image',
             'typologies' => 'required'
+        ];
+    }
+
+    public function messages() : array
+    {
+        return [
+            'name.required' => "Il ristorante deve avere un nome",
+            'name.min' => "Il nome del ristorante deve essere di almeno :min caratteri",
+            'address.required' => "L'indirizzo del ristorante è richiesto",
+            'address.max' => "L'indirizzo non può essere più lungo di :max caratteri",
+            'name.max' => "Il nome del ristorante non può essere più lungo di :max caratteri",
+            'piva.required' => 'Inserisci una partita IVA',
+            'piva.numeric' => 'Formato partita IVA non corretto',
+            'photo.required' => 'Inserisci una foto per il ristorante',
+            'photo.image' => 'Puoi caricare solo file immagini',
+            'typologies.required' => 'Almeno una categoria per il ristorante è richiesta'
+
         ];
     }
 }
